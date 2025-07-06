6be0fa4d8acee9b82131577d369226029b9eb80a
larinpaul <www.larinpavel@mail.ru>
2025-07-06 20:18:47 +0300

    from pydantic import BaseModel, ConfigDict
    class User(BaseModel):
        id: int
        name: str = 'Jane Doe'
    
        model_config = ConfigDict(str_max_length=10)

--- models.py
+++ models.py
@@ -18,3 +18,19 @@
 # will conform to the field types defined on the model
 
 
+# Basic model usage
+
+# Pydantic relies heavily on the existing Python typing constucts to define models.
+# If you are not familiar with those, the following resources can be useful:
+# * The Type System Guides // https://typing.python.org/en/latest/guides/index.html
+# * The mypy documentation // https://mypy.readthedocs.io/en/latest/
+
+from pydantic import BaseModel, ConfigDict
+
+
+class User(BaseModel):
+    id: int
+    name: str = 'Jane Doe'
+
+    model_config = ConfigDict(str_max_length=10)
+    
