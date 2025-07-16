2d21488089f0c82d64cc8079279fa0799c8178bd
larinpaul <www.larinpavel@mail.ru>
2025-07-16 07:37:09 +0300

    bones = PetCls(name='Bones', species='dog') orion = PetCls(name='Orion', species='cat') anna = PersonCls(name='Anna', age=20, pets=[bones, orion]) anna_model = Person.model_validate(anna) print(anna_model) """ name='Anna' age = 20.0 pets=[Pet(name='Bones', species='dog'), Pet(name='Orion', species='cat')] """

--- models.py
+++ models.py
@@ -360,5 +360,7 @@ bones = PetCls(name='Bones', species='dog')
 orion = PetCls(name='Orion', species='cat')
 anna = PersonCls(name='Anna', age=20, pets=[bones, orion])
 anna_model = Person.model_validate(anna)
-
-
+print(anna_model)
+"""
+name='Anna' age = 20.0 pets=[Pet(name='Bones', species='dog'), Pet(name='Orion', species='cat')]
+"""
