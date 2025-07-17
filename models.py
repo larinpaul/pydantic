ea233664466a6d9e47e9509c1d3145772081accb
larinpaul <www.larinpavel@mail.ru>
2025-07-17 07:23:19 +0300

    class Model(BaseModel):     list_of_ints: list[int]     a_float: float
    data = dict(
        list_of_ints=['1', 2, 'bad'],
        a_float='not a float',
    )
    
    try:
        Model(**data)
    except ValidationError as e:
        print(e)
        """
        2 validation errors for Model
        list_of_ints.2
            Input should be a valid integer, unable to parse string as an integer [type=int_parsing, input_value='bad', input_type=str]
        a_float
            Input should be a valid number, unable to parse string as a number [type=float_parsing, input_value='not a float', input_type=str]
        """

--- models.py
+++ models.py
@@ -371,4 +371,23 @@ name='Anna' age = 20.0 pets=[Pet(name='Bones', species='dog'), Pet(name='Orion',
 
 from pydantic import BaseModel, ValidationError
 
+class Model(BaseModel):
+    list_of_ints: list[int]
+    a_float: float
 
+data = dict(
+    list_of_ints=['1', 2, 'bad'],
+    a_float='not a float',
+)
+
+try:
+    Model(**data)
+except ValidationError as e:
+    print(e)
+    """
+    2 validation errors for Model
+    list_of_ints.2
+        Input should be a valid integer, unable to parse string as an integer [type=int_parsing, input_value='bad', input_type=str]
+    a_float
+        Input should be a valid number, unable to parse string as a number [type=float_parsing, input_value='not a float', input_type=str]    
+    """
