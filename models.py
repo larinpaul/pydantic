b6e46db970802e516ccead04c4d6a8c66cfebb01
larinpaul <www.larinpavel@mail.ru>
2025-07-26 09:10:17 +0300

    # ALWAYS revalidate_instances='always'
    from pydantic import BaseModel, ConfigDict, ValidationError
    
    class Model(BaseModel):
        a: int
    
        model_config = ConfigDict(revalidate_instances='always')
    
    m = Model(a=0)
    # note: setting `validate_assignment` to `True` in the config can prevent this kind of misbehavior.
    m.a = 'not an int'
    
    try:
        m2 = Model.model_validate(m)
    except ValidationError as e:
        print(e)
        """
        1 validation error for Model
        a
            Input should be a valid integer, unable to parse string as an integer [type=int_parsing, input_value='not an int', input_type=str]
        """

--- models.py
+++ models.py
@@ -485,8 +485,28 @@ m.a = 'not an int'
 m2 = Model.model_validate(m)
 
 
+# ALWAYS revalidate_instances='always'
+
+from pydantic import BaseModel, ConfigDict, ValidationError
 
 
+class Model(BaseModel):
+    a: int
 
+    model_config = ConfigDict(revalidate_instances='always')
+
+m = Model(a=0)
+# note: setting `validate_assignment` to `True` in the config can prevent this kind of misbehavior.
+m.a = 'not an int' 
+
+try:
+    m2 = Model.model_validate(m)
+except ValidationError as e:
+    print(e)
+    """
+    1 validation error for Model
+    a
+        Input should be a valid integer, unable to parse string as an integer [type=int_parsing, input_value='not an int', input_type=str]
+    """
 
 
