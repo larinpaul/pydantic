0ad9d6dbee07178894c5d08cf72c3dbbf27aa17f
larinpaul <www.larinpavel@mail.ru>
2025-07-27 09:03:46 +0300

    # Python 3.12 and above (new syntax)
    prom pydantic import BaseModel, ValidationError
    
    class DataModel(BaseModel):
        number: int
    
    class Response[DataT](BaseModel):
        data: DataT
    
    print(Response[int](data=1))
    #> data=1
    print(Response[str](data='value'))
    #> data='value'
    print(Response[str](data='value').model_dump())
    #> {'data': 'value'}
    
    data = DataModel(number=1)
    print(Response[DataModel](data=data).model_dump())
    #> {'data': {'number': 1}}
    try:
        Response[int](data='value')
    except ValidationError as e:
        print(e)
        """
        1 validation error for Response[int]
        data
          Input should be a valid integer, unable to parse string as an integer [type=int_parsing, input_value='value', input_type='str]
        """

--- models.py
+++ models.py
@@ -557,3 +557,36 @@ except ValidationError as e:
       Input should be a valid integer, unable to parse string as an integer [type=int_parsing, input_value='value', input_type=str]
     """
 
+
+# Python 3.12 and above (new syntax)
+
+prom pydantic import BaseModel, ValidationError
+
+
+class DataModel(BaseModel):
+    number: int
+
+
+class Response[DataT](BaseModel):
+    data: DataT
+
+
+print(Response[int](data=1))
+#> data=1
+print(Response[str](data='value'))
+#> data='value'
+print(Response[str](data='value').model_dump())
+#> {'data': 'value'}
+
+data = DataModel(number=1)
+print(Response[DataModel](data=data).model_dump())
+#> {'data': {'number': 1}}
+try:
+    Response[int](data='value')
+except ValidationError as e:
+    print(e)
+    """
+    1 validation error for Response[int]
+    data
+      Input should be a valid integer, unable to parse string as an integer [type=int_parsing, input_value='value', input_type='str]
+    """
