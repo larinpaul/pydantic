c2a3b968b591027938c3448e6bb578ede2887f83
larinpaul <www.larinpavel@mail.ru>
2025-07-27 08:55:37 +0300

    from typing import Generic, TypeVar
    from pydantic import BaseModel, ValidationError
    
    DataT = TypeVar('DataT')
    
    class DataModel(BaseModel):
        number: int
    
    class Response(BaseModel, Generic[DataT]):
        data: DataT
    
    print(Response[int](data=1))
    #> data=1
    print(Response[str](data='value'))
    #> data='value'
    print(Response[str](data='value').model_dump())
    #> {'data': 'value'}
    
    data = DataModel(number=1)
    print(Response[DataModel](data=data).model_dump())
    #> {'data': {'number': 1}}
    try:
        Response[int](data='value')
    try:
        Response[int](data='value')
    except ValidationError as e:
        print(e)
        """
        1 validation error for Response[int]
        data
          Input should be a valid integer, unable to parse string as an integer [type=int_parsing, input_value='value', input_type=str]
        """

--- models.py
+++ models.py
@@ -517,7 +517,43 @@ except ValidationError as e:
 # by PEOP 695 in Python 3.12) and the old syntax are supported
 # (refer to the Python documentation for more details).
 
+# Here is an example using a generic Pydantic model
+# to create an easily-reused HTTP response payload wrapper.
+
+from typing import Generic, TypeVar
+
+from pydantic import BaseModel, ValidationError
+
+DataT = TypeVar('DataT')
 
 
+class DataModel(BaseModel):
+    number: int
 
+
+class Response(BaseModel, Generic[DataT]):
+    data: DataT
+
+
+print(Response[int](data=1))
+#> data=1
+print(Response[str](data='value'))
+#> data='value'
+print(Response[str](data='value').model_dump())
+#> {'data': 'value'}
+
+data = DataModel(number=1)
+print(Response[DataModel](data=data).model_dump())
+#> {'data': {'number': 1}}
+try:
+    Response[int](data='value')
+try:
+    Response[int](data='value')
+except ValidationError as e:
+    print(e)
+    """
+    1 validation error for Response[int]
+    data
+      Input should be a valid integer, unable to parse string as an integer [type=int_parsing, input_value='value', input_type=str]
+    """
 
