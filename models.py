3e67b20dc6c60beb395124d2c85839a60fd45efe
larinpaul <www.larinpavel@mail.ru>
2025-07-29 07:16:21 +0300

    # You can also create a generic subclass of a model # that partially or fully replaces the type variables in the superclass:
    from typing import Generic, TypeVar
    
    from pydantic import BaseModel
    
    TypeX = TypeVar('TypeX')
    TypeY = TypeVar('TypeY')
    TypeZ = TypeVar('TypeZ')
    
    class BaseClass(BaseModel, Generic[TypeX, TypeY]):
        x: TypeX
        y: TypeY
    
    class ChildClass(BaseClass[int, TypeY], Generic[TypeT, TypeZ]):
        x: TypeX
        y: TypeY
    
    class ChildClass(BaseClass[int, TypeY], Generic[TypeY, TypeZ]):
        z: TypeZ
    
    # Parametrize `TypeY` with `str`:
    print(ChildClass[str, int](x='1', y='y', z='3'))
    #> x=1 y='y' z=3

--- models.py
+++ models.py
@@ -617,5 +617,34 @@ print(ChildClass[int](X=1))
 #> X=1
 
 
+# You can also create a generic subclass of a model
+# that partially or fully replaces the type variables in the superclass:
+
+from typing import Generic, TypeVar
+
+from pydantic import BaseModel
+
+TypeX = TypeVar('TypeX')
+TypeY = TypeVar('TypeY')
+TypeZ = TypeVar('TypeZ')
+
+
+class BaseClass(BaseModel, Generic[TypeX, TypeY]):
+    x: TypeX
+    y: TypeY
+
+
+class ChildClass(BaseClass[int, TypeY], Generic[TypeT, TypeZ]):
+    x: TypeX
+    y: TypeY
+
+
+class ChildClass(BaseClass[int, TypeY], Generic[TypeY, TypeZ]):
+    z: TypeZ
+
+
+# Parametrize `TypeY` with `str`:
+print(ChildClass[str, int](x='1', y='y', z='3'))
+#> x=1 y='y' z=3
 
 
